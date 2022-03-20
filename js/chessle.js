e4b0ca2fc3818ff325210c56aca617ceb88ba703
Georgios Kousouris <georgios@yelp.com>
2022-03-20 16:25:20 +0000

    Olly*

--- js/chessle.js
+++ js/chessle.js
@@ -1,6 +1,9 @@
+// const SERVER_URL_GET = 'https://d1vwq1uqg5c4bn.cloudfront.net/';
 const SERVER_URL = 'www.google.com';
 
 const MAX_GUESSES = 10;
+const CURR_LENGTH = 20;
+const TODAY = '2022-03-20';
 
 const LIGHT_BOX = 'bg-dark';
 const BLACK_BOX = 'text-white bg-secondary';
@@ -8,8 +11,8 @@ const YELLOW_BOX = 'text-white bg-warning';
 const GREEN_BOX = 'text-white bg-success';
 
 const DIFFICULTY_NAME_MAP = {
-    'n': 'Msod mode',
-    'e': 'Msod mode'
+    'n': 'Normal',
+    'e': 'Expert'
 };
 
 const RESULT_TO_BOX_MAP = {
@@ -26,8 +29,7 @@ const RESULT_TO_EMOJI_MAP = {
 
 const DIFFICULTY_TO_LENGTH_MAP = {
     'n': 6,
-    'e': 10,
-    'msod': 12
+    'e': 10
 };
 
 const GUESS_BOX_SIZE_TO_FONT_WEIGHT = {
@@ -78,14 +80,16 @@ const GUESS_BOX_SIZE_TO_FONT_SIZE = {
 let difficulty = 'e';
 let date;
 let chessleNum;
-let ans_name_normal;
-let ans_name_expert;
-let full_ans;
+let ansNameNormal;
+let ansNameExpert;
+let fullAns;
 let ans;
+let chessableLink;
 let userId;
 
 let prevGuesses = [];
 let prevResults = [];
+let curGuessStack = [];
 let isGameOver = false;
 
 let percentCorrect;
@@ -110,7 +114,7 @@ function submitGuess() {
     updateLocalStorage();
 
     // Add guess and result to new row
-    addPrevGuessHtml(guess, result)
+    addPrevGuessHtml(guess, result);
 
     // Is the guess correct?
     if (result.every(val => val === 'g')) {
@@ -128,7 +132,7 @@ function submitGuess() {
 }
 
 function getAnsLength() {
-    return 20;
+    return CURR_LENGTH;
 }
 
 function updateLocalStorage() {
@@ -147,12 +151,10 @@ function gameOver(isWin) {
     document.getElementById('submit').style.display = 'none';
     document.getElementById('undoButton').style.display = 'none';
     document.getElementById('flipButton').style.display = 'none';
+    document.getElementById('fillButton').style.display = 'none';
     isGameOver = true;
     localStorage.setItem('isWin', isWin);
     updateLocalStorage();
-
-    game.load_pgn(ans.join(' '), {sloppy: true});
-    board.position(game.fen());
 
     sendResult(isWin);
 
@@ -195,6 +197,26 @@ function check(guess, answer) {
     return result;
 }
 
+function fillPrevCorrect() {
+    if (prevResults.length > 0) {
+        let mostRecentResult = prevResults[prevResults.length - 1];
+        let numCorrect = 0;
+        for (let i = 0; i < mostRecentResult.length; i++) {
+            if (mostRecentResult[i] === 'g') {
+                numCorrect++;
+            } else {
+                break;
+            }
+        }
+        if (numCorrect > 0) {
+            let prevCorrectGuesses = prevGuesses[prevGuesses.length - 1].slice(0, numCorrect);
+            game.load_pgn(prevCorrectGuesses.join(' '), {sloppy: true});
+            board.position(game.fen());
+            populateGuessBoxes(game.history());
+        }
+    }
+}
+
 function createEmojiPattern(separator) {
     let pattern = ''
     for (let i = 0; i < prevResults.length; i++) {
@@ -215,14 +237,15 @@ function createSharePasta() {
         + tries
         + '/' + MAX_GUESSES + '\n\n'
         + createEmojiPattern('\n') + '\n'
-        + 'https://geokous.github.io/';
+        + 'https://jackli.gg/chessle';
     let shareData = {
         text: pasta
     };
     // From detectmobilebrowsers.com
     let isMobile = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) isMobile = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    if (isMobile) {
+    if (isMobile && (navigator.userAgent.toLowerCase().indexOf('firefox') === -1)
+            && navigator.canShare && navigator.canShare(shareData)) {
         navigator.share(shareData);
     } else {
         navigator.clipboard.writeText(pasta);
@@ -347,18 +370,14 @@ function populateGuessBoxes(moves) {
     $('#currentGuess').html(guessHtml);
 }
 
-function chooseNormalMode() {
-    difficulty = 'n';
-    setUpAnswer();
-}
-
-function chooseExpertMode() {
-    difficulty = 'e';
+function chooseMode(chosenDifficulty) {
+    difficulty = chosenDifficulty;
     setUpAnswer();
 }
 
 function setUpAnswer() {
-    ans = full_ans.slice(0, getAnsLength());
+    console.log(fullAns)
+    ans = fullAns.slice(0, getAnsLength());
     populateGuessBoxes([]);
     $('#instructionsModal').modal('hide');
 }
@@ -366,7 +385,7 @@ function setUpAnswer() {
 function openInstructionsModal() {
     $('#instructionsModalTitle').text('Welcome to Chessle');
     let instructionsText = `
-        Guess the entire opening sequence, for both white and black!<br />
+        To have come here, you probably know what to do!<br />
         <div class="instructions-example">
             <p class="instructions-example-title">Example</p>
             <img class="instructions-example-img" src="img/example.png">
@@ -392,6 +411,14 @@ function openInstructionsModal() {
         keyboard: false
     });
     $('#instructionsModal').modal('show');
+}
+
+function openMaintenanceModal() {
+    $('#maintenanceModal').modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+    $('#maintenanceModal').modal('show');
 }
 
 function createPgnFromMoveList(moves) {
@@ -429,22 +456,24 @@ function openShareModal(text) {
         </p>
     `;
     $('#shareModalBody').html(modalBody);
+    $('#chessableLearnMoreButton').attr('href', chessableLink);
+    $('#chessableLearnMoreButton').click(function() {
+        logOutboundClick(chessableLink);
+    });
     $('#shareModal').modal('show');
 }
 
 
 // HTTP requests
 
-
 function getAnswer() {
-    let game = '1. e4 c5 2. Nf3 Nc6 3. c3 Nf6 4. e5 Nd5 5. d4 cxd4 6. cxd4 d6 7. Bc4 Nb6 8. Bb5 dxe5 9. Nxe5 Bd7 10. Nxd7 Qxd7'
-    let hardcoded_ans = '{"date": "2022-02-22", "num": 6, "name_normal": "Sicilian Defense: Old Sicilian"}'
+    let game = '1. e4 d6 2. d4 Nf6 3. Nc3 c6 4. f4 Qa5 5. Bd3 e5 6. Nf3 Bg4 7. Be3 Nbd7 8. O-O Be7 9. h3 Bxf3 10. Qxf3 O-O'
+    let hardcoded_ans = '{"date": "2022-02-22", "num": 6, "name": "Sicilian Defense: Old Sicilian"}'
     let res = JSON.parse(hardcoded_ans);
-    date = res.date;
+    date = TODAY;
     chessleNum = res.num;
-    ans_name_normal = res.name_normal;
-    ans_name_expert = ""
-    full_ans = game.replace(/ [0-9]+[.]/g, '').slice(3).split(' ');
+    ans_name_normal = res.name;
+    fullAns = game.replace(/ [0-9]+[.]/g, '').slice(3).split(' ');
 
 }
 
@@ -465,9 +494,26 @@ function sendResult(isWin) {
             let res = JSON.parse(xhttp.responseText);
             percentCorrect = res.percent_correct;
             avgTries = res.avg_tries;
+            $('#share-modal-stats').html(`
+                <br />
+                Today, ${percentCorrect}% of people got ${DIFFICULTY_NAME_MAP[difficulty]} mode correct.
+                <br />
+                Of the people who got it correct, it took ${avgTries} tries on average.
+                <br />`);
         }
     };
-    xhttp.open('POST', SERVER_URL, false);
+    xhttp.open('POST', SERVER_URL, true);
+    xhttp.send(body);
+}
+
+function logOutboundClick(link) {
+    let xhttp = new XMLHttpRequest();
+    let body = JSON.stringify({
+        'userId': userId,
+        'date': date,
+        'link': link
+    });
+    xhttp.open('POST', SERVER_URL + 'outbound_click', false);
     xhttp.send(body);
 }
 
@@ -496,6 +542,10 @@ function onDrop(source, target) {
 
     if (move === null) return 'snapback';
 
+    if (move.san !== curGuessStack.pop()) {
+        curGuessStack = [];
+    }
+
     populateGuessBoxes(game.history());
 }
 
@@ -509,7 +559,10 @@ function resetBoard() {
 }
 
 function undo() {
-    game.undo();
+    let undoedMove = game.undo();
+    if (undoedMove !== null) {
+        curGuessStack.push(undoedMove.san);
+    }
     syncGameBoard();
 }
 
@@ -543,9 +596,13 @@ function setUp() {
     } else {
         userId = localStorage.getItem('userId');
     }
-    getAnswer();
-    console.log('hi')
-    if ('2022-02-22' !== localStorage.getItem('date')) {
+    try {
+        getAnswer();
+    } catch (e) {
+        openMaintenanceModal();
+        return;
+    }
+    if (date !== localStorage.getItem('date')) {
         localStorage.setItem('prevGuesses', '[]');
         localStorage.setItem('prevResults', '[]');
         localStorage.setItem('isGameOver', false);
@@ -565,6 +622,41 @@ function setUp() {
         }
     }
 }
+
+$(document).on("keydown", function (e) {
+    switch (e.keyCode) {
+        case 8:
+        case 37:
+            if (!isGameOver) {
+                undo();
+            }
+            break;
+        case 32:
+            if (!isGameOver) {
+                fillPrevCorrect();
+            }
+            e.preventDefault();
+            e.stopPropagation();
+            break;
+        case 39:
+            if (!isGameOver) {
+                if (curGuessStack.length > 0) {
+                    game.move(curGuessStack.pop());
+                    syncGameBoard();
+                } else if (document.getElementById('guess-0').innerHTML === '') {
+                    fillPrevCorrect();
+                }
+            }
+            break;
+        case 13:
+            if (!isGameOver) {
+                submitGuess();
+            }
+        default:
+            break;
+    }
+});
+
 
 initializeBoard();
 setUp();
